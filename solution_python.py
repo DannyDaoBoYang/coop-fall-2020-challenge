4df32d14736f706522b70a198619cd7e34f7768f
DannyDaoBoYang <34634047+DannyDaoBoYang@users.noreply.github.com>
2020-07-10 14:14:26 -0400

    Student
    Is this too early for spring 2021 co-op?

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,49 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.did=[]
+        self.undid=[]
 
     def add(self, num: int):
-        pass
+        self.value += num
+        self.did.append([1,num])
+        #self.undid = [] ok so this is not needed wtf... change does not clear cache of undo
+
 
     def subtract(self, num: int):
-        pass
+        self.value -= num
+        self.did.append([0,num])
+        #self.undid = [] ok so this is not needed wtf...
 
     def undo(self):
-        pass
+        if(len(self.did)):
+            change = self.did[-1]
+            self.did.pop()
+            op = change[0]
+            num = change[1]
+            if(op == 0): # subtract so we add
+                self.value += num
+            else:
+                self.value -= num
+            self.undid.append(change)
+
 
     def redo(self):
-        pass
+        if(len(self.undid)):
+             change = self.undid[-1]
+             self.undid.pop()
+             op = change [0]
+             num = change [1]
+             if(op == 1): # add so add
+                self.value += num
+             else:
+                self.value -= num
+             self.did.append(change)
 
     def bulk_undo(self, steps: int):
-        pass
+        for i in range (0, steps):
+            self.undo()
 
     def bulk_redo(self, steps: int):
-        pass
+        for i in range (0, steps):
+            self.redo()
